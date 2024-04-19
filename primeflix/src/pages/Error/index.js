86762220619eb332237671822522d890c478d493
SesiSenai1DE <130493869+SesiSenai1DE@users.noreply.github.com>
2024-04-19 15:41:03 -0300

    Finalização do exercício.
    Finalização do exercício.

--- primeflix/src/pages/Error/index.js
+++ primeflix/src/pages/Error/index.js
@@ -4,4 +4,17 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRotateBackward } from '@fortawesome/free-solid-svg-icons'; 
 import styles from './styles.module.css'; 
 
-// 4ª Digitação (Aqui)
+function Error() { // Define o componente funcional Error.
+  return (
+    <div className={styles.not_found}> {/* Define um contêiner com a classe CSS 'not_found' */}
+			<h1>404</h1> {/* Renderiza o título '404' */}
+      <h2>Oops! Página não encontrada</h2> {/* Renderiza a mensagem de erro 'Oops! Página não encontrada' */}
+			<Link to='/'> {/* Renderiza um link para a página inicial */}
+				<FontAwesomeIcon icon={faArrowRotateBackward} size='lg'/> {/* Renderiza o ícone de seta */}
+        Veja todos os filmes {/* Renderiza o texto do link */}
+			</Link> {/* Fecha o link */}
+    </div>
+  );
+}
+
+export default memo(Error); // Exporta o componente Error, envolvido pela função `memo` para otimização de renderização.
